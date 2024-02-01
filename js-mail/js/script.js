44f5733352799836e8c78d2069e2945997eb6cd7
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-01 17:18:34 +0100

    email js done

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,13 +1,29 @@
-//Chiedi all’utente la sua email,
-//controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare da noi un array di email casuali),
-//stampa un messaggio appropriato sull’esito del controllo.
-//(NON utilizzate il metodo .includes())
-//Prima di partire a scrivere codice poniamoci qualche domanda:
-//Che ci sia un array da qualche parte?
-//Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
 
 //creare array lista di email che possono accedere
-//chiedere all'utente la sua email
-//controllo
-//stampa messaggio esito
+const buttonElement = document.getElementById("run");
+
+buttonElement.addEventListener("click",
+    function () {
+        const authorizedEmails = ["topolino@gmail.com", "paperino@gmail", "pluto@gmail.com"];
+
+
+        //chiedere all'utente la sua email
+        const userEmail = document.getElementById("insert-email").value;
+
+        //controllo
+        let authorized = false;
+
+        for (let i = 0; i < authorizedEmails.length; i++) {
+            if (userEmail === authorizedEmails[i]) {
+                authorized = true;
+            }
+        }
+
+        //stampa messaggio esito
+        if (authorized) {
+            document.getElementById("control-result").innerText = "Email autorizzata all'accesso.";
+        } else {
+            document.getElementById("control-result").innerText = "Email non autorizzata all'accesso. Si prega di inserire una email autorizzata.";
+        }
+    }
+)
