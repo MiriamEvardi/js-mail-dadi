ed12cdfbcf1ee9ebf920b53c8fb53c55e804da3b
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-01 16:47:59 +0100

    start js-email

---
+++ js-mail/js/script.js
@@ -0,0 +1,13 @@
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare da noi un array di email casuali),
+//stampa un messaggio appropriato sull’esito del controllo.
+//(NON utilizzate il metodo .includes())
+//Prima di partire a scrivere codice poniamoci qualche domanda:
+//Che ci sia un array da qualche parte?
+//Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+//creare array lista di email che possono accedere
+//chiedere all'utente la sua email
+//controllo
+//stampa messaggio esito
