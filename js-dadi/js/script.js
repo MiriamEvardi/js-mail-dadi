43cb1d9f769a4fc11867f5f96cffbf6104a1c833
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-01 18:02:05 +0100

    js-dadi fixed

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -2,25 +2,20 @@ const buttonElement = document.getElementById("run");
 
 buttonElement.addEventListener("click",
     function () {
-        const userChoice = document.getElementById("number").value;
-        const computerNumber = Math.floor(Math.random() * 6 + 1);
-        const userNumbers = [1, 2, 3, 4, 5, 6];
 
-        document.getElementById("user").innerText = "Hai scelto: " + userNumbers[userChoice - 1];
+        const computerNumber = Math.floor(Math.random() * 6 + 1);
+        const userNumbers = Math.floor(Math.random() * 6 + 1);
+
+        document.getElementById("user").innerText = "Hai scelto: " + userNumbers;
         document.getElementById("computer").innerText = "Il computer ha scelto: " + computerNumber;
 
-        // Controllo numero
-        if (userChoice < 1 || userChoice > 6) {
-            document.getElementById("invalid-choice").innerText = "Numero non valido. Inserisci un valore da 1 a 6."
-        }
-
         //SE lo user fa un punteggio superiore a computer VINCE
-        if (userChoice > computerNumber) {
+        if (userNumbers > computerNumber) {
 
             document.getElementById("result").innerText = "Hai VINTO!";
 
             //ALTRIMENTI SE ha un numero inferiore lo user PERDE
-        } else if (userChoice < computerNumber) {
+        } else if (userNumbers < computerNumber) {
 
             document.getElementById('result').innerHTML = "Fai schifo, cambia gioco..";
 
